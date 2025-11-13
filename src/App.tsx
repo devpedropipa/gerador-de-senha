1fcb92c1cdabe8aaa1b494d33fcb37785588a276
devpedropipa <phpipa2006@gmail.com>
2025-11-12 21:12:47 -0300

    Estilizando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,90 @@
+{
+    /* Dependências */
+}
 import { useState } from "react";
 
-import copyImg from "./assets/copiar.png"
+{
+    /* Arquivos de estilo */
+}
+import "./styles/gerador.css";
 
+{
+    /* Imagens */
+}
+import copyImg from "./assets/copiar.png";
+
+{
+    /* Componente */
+}
 function App() {
     const [quantCar, setQuantCar] = useState(8);
 
     return (
-        <main id="conteudo">
+        <main id="conteudo-site">
             <h1>Gerador de Senhas</h1>
 
             {/* Gerador de senhas */}
             <div id="card">
-                <div>
-                    <input type="text" name="input-senha" id="senha-gerada" />
-                    <img src={copyImg} alt="ícone-copiar" id="img-copiar" />
-                </div>
-
-                {/* Quantidade de caracteres */}
-                <div id="container-range">
-                    <input
-                        type="range"
-                        name="input-caract"
-                        id="quant-caracteres"
-                        min={4}
-                        max={20}
-                        value={quantCar}
-                        onChange={(e) => setQuantCar(Number(e.target.value))}
-                    />
-                    <input
-                        type="number"
-                        name="input-number"
-                        id="val-quant-caracteres"
-                        value={String(quantCar)}
-                    />
-                </div>
-
-                {/* Configuração do gerador */}
-                <div id="container-config">
-                    <div>
-                        <input
-                            type="checkbox"
-                            name="input-check"
-                            className="check-config"
-                        />
-                        <span>Letras maiúsculas</span>
+                <main id="conteudo-card">
+                    <div id="container-senha">
+                        <input type="text" name="input-senha" id="senha-gerada" readOnly />
+                        <img src={copyImg} alt="ícone-copiar" id="img-copiar" />
                     </div>
-
-                    <div>
+                    {/* Quantidade de caracteres */}
+                    <div id="container-range">
                         <input
-                            type="checkbox"
-                            name="input-check"
-                            className="check-config"
+                            type="range"
+                            name="input-caract"
+                            id="input-range"
+                            min={4}
+                            max={20}
+                            value={quantCar}
+                            onChange={(e) => setQuantCar(Number(e.target.value))}
                         />
-                        <span>Letras minúsculas</span>
-                    </div>
-
-                    <div>
                         <input
-                            type="checkbox"
-                            name="input-check"
-                            className="check-config"
+                            type="number"
+                            name="input-number"
+                            id="quant-caracteres"
+                            value={String(quantCar)}
                         />
-                        <span>Números</span>
                     </div>
-
-                    <div>
-                        <input
-                            type="checkbox"
-                            name="input-check"
-                            className="check-config"
-                        />
-                        <span>Símbolos</span>
+                    {/* Configuração do gerador */}
+                    <div id="container-config">
+                        <div>
+                            <input
+                                type="checkbox"
+                                name="input-check"
+                                className="check-config"
+                            />
+                            <span className="check-nome">Letras maiúsculas</span>
+                        </div>
+                        <div>
+                            <input
+                                type="checkbox"
+                                name="input-check"
+                                className="check-config"
+                            />
+                            <span className="check-nome">Letras minúsculas</span>
+                        </div>
+                        <div>
+                            <input
+                                type="checkbox"
+                                name="input-check"
+                                className="check-config"
+                            />
+                            <span className="check-nome">Números</span>
+                        </div>
+                        <div>
+                            <input
+                                type="checkbox"
+                                name="input-check"
+                                className="check-config"
+                            />
+                            <span className="check-nome">Símbolos</span>
+                        </div>
                     </div>
-                </div>
-                <button id="gerar-senha">Gerar Senha</button>
+                    <button id="gerar-senha">Gerar Senha</button>
+                </main>
             </div>
         </main>
     );
